8352c56f61083d9828b42150581142915c707023
Samuel <samuelbeltran048@gmail.com>
2025-04-30 15:08:45 -0500

    Se arreglo el error de crear Usuario

--- src/Modelo/CrearUsuario.java
+++ src/Modelo/CrearUsuario.java
@@ -1,62 +1,26 @@
 package Modelo;
 
-public class CrearUsuario  {
-    private String nombre, apellido, telefono, direccion, usuario, contraseña;
-    public CrearUsuario(String nombre, String apellido, String telefono, String direccion, String usuario, String contraseña) {
+public class CrearUsuario {
+    private String nombre;
+    private String apellido;
+    private String telefono;
+    private String direccion;
+    private String usuario;
+    private String contrasena;
+
+    public CrearUsuario(String nombre, String apellido, String telefono, String direccion, String usuario, String contrasena) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.telefono = telefono;
         this.direccion = direccion;
         this.usuario = usuario;
-        this.contraseña = contraseña;
+        this.contrasena = contrasena;
     }
 
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
-    public String getDireccion() {
-        return direccion;
-    }
-
-    public void setDireccion(String direccion) {
-        this.direccion = direccion;
-    }
-
-    public String getUsuario() {
-        return usuario;
-    }
-
-    public void setUsuario(String usuario) {
-        this.usuario = usuario;
-    }
-
-    public String getContraseña() {
-        return contraseña;
-    }
-
-    public void setContraseña(String contraseña) {
-        this.contraseña = contraseña;
-    }
+    public String getNombre() { return nombre; }
+    public String getApellido() { return apellido; }
+    public String getTelefono() { return telefono; }
+    public String getDireccion() { return direccion; }
+    public String getUsuario() { return usuario; }
+    public String getContrasena() { return contrasena; }
 }
