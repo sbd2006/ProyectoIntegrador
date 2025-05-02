8f6027defc075110a37720add3e0b4496984c029
Santi104 <monzonedi05@gmail.com>
2025-05-02 17:19:00 -0500

    Se hicieron cambios en los de regresar de algunas clases, mas el boton de cerrar sesion de empleado que no funcionaba

--- src/Modelo/CrearUsuarioDAO.java
+++ src/Modelo/CrearUsuarioDAO.java
@@ -8,13 +8,7 @@ public class CrearUsuarioDAO {
 
     private final String URL = "jdbc:mysql://127.0.0.1:3306/postresmariajose";
     private final String USER = "root";
-    private final String PASSWORD = "OH{c<6H1#cQ%F69$i";
-
-
-
-    private final String URL = "jdbc:mysql://127.0.0.1:3306/postresmariajose";
-    private final String USER = "root";
-    private final String PASSWORD = "Juanguis-2006";
+    private final String PASSWORD = "Santi104";
 
     public boolean guardarUsuario(CrearUsuario usuario) {
         String sql = "INSERT INTO usuarios (Nombre, Apellido, Telefono, Direccion, Usuario, Pass) VALUES (?, ?, ?, ?, ?, ?)";
--- src/Vista/EmpleadoVista.java
+++ src/Vista/EmpleadoVista.java
@@ -1,7 +1,9 @@
 package Vista;
 
 import Controlador.ControladorIS;
+import Controlador.VentaControlador;
 import Modelo.ModeloIS;
+import Modelo.VentaDAO;
 
 import javax.swing.*;
 
@@ -10,13 +12,13 @@ public class EmpleadoVista extends JFrame {
     public JButton realizarVentaButton;
     public JButton cerrarSesionButton;
     private JLabel Bienvenido;
-    private JLabel lblBienvenida;
 
     public EmpleadoVista() {
         setTitle("Vista Empleado");
         setContentPane(PantallaCliente);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         pack();
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
     }
 
     public void setNombreUsuario(String nombreUsuario) {
@@ -34,5 +36,19 @@ public class EmpleadoVista extends JFrame {
 
     public void mostrarEmpleado() {
         setVisible(true);
+    }
+
+
+    public void mostrarVenta() {
+        VentaVista vistaV = new VentaVista();
+        VentaDAO dao = new VentaDAO();
+        VentaControlador controlador = new VentaControlador(vistaV, dao, this);
+        vistaV.setVisible(true);
+        this.setVisible(false);
+    }
+
+
+    public void cerrarEmpleado() {
+        dispose();
     }
 }
